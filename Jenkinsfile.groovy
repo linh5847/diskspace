624ff45c8b50dadeef61ffbda77b8f4f64e59714
linh.nguyen <linh.nguyen@hmcts.net>
2021-02-22 20:57:28 +0000

    update

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,37 @@
+#!/usr/bin/env groovy
+
+pipeline {
+  agent any
+
+  options {
+    ansiColor('xterm')
+  }
+
+  // parameters {
+  //   choice(name: 'CHOICE', choices: ['devccm01', 'devccm02', 'devccm03', 'devccm04', 'devccm05'], description: 'select an options')
+  // }
+
+  environments {
+    azure {
+
+      teamStacks = [
+        [
+          name: 'ccm'
+          stacks: [
+            [ name: 'steccm01'],
+            [ name: 'steccm02']
+          ]
+        ],
+        [
+          name: 'bulk-scan'
+          stacks: [
+            [ name: 'devscan01'],
+            [ name: 'devscan02']
+          ]
+        ]
+      ]
+    }
+  }
+
+   
+}
