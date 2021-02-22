48dd936e24289b757c643437a0e65de5cb2c3ab9
linh.nguyen <linh.nguyen@hmcts.net>
2021-02-22 23:57:55 +0000

    updates

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,23 +7,6 @@ pipeline {
     ansiColor('xterm')
   }
 
-  // parameters {
-  //   choice(name: 'CHOICE', choices: ['devccm01', 'devccm02', 'devccm03', 'devccm04', 'devccm05'], description: 'select an options')
-  // }
-
-  environment {
-    ste = 'steccm01'
-    dev = 'devccm01'
-  }
-
-  parameters {
-    choice(name: 'CHOICE', choices: ['ste', 'dev'], description: 'select an options')
-  }
-
-  // parameters {
-  //   string(name: 'JENKINS_ANSIBLE_HOST_LIMIT', defaultValue: '', description: 'user choice')
-  // }
-
   stages {
 
     stage('Checkout git repo') {
@@ -37,14 +20,7 @@ pipeline {
     stage('Checking diskspace and cleanup files that are 1 day or olders.') {
       steps {
         script {
-
-          if('${params.CHOICE}' == 'ste') {
-            ansiblePlaybook inventory: '', tags: 'diskspace_and_delete_files', playbook: 'site.yml', sudo: true, sudoUser: 'jenkins'
-          }
-
-          if('${params.CHOICE}' == 'dev') {
-            ansiblePlaybook inventory: '', tags: 'diskspace_and_delete_files', playbook: 'site.yml', sudo: true, sudoUser: 'jenkins'
-          }
+          ansiblePlaybook colorized: true, playbook: 'site.yml'
         }
       }
     }
